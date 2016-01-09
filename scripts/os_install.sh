61dad8ceb9549e1d4d778030fc13b7bdcff85ce5
Paco pac <pakokrew@gmail.com>
2016-01-09 15:54:01 +0100

    Instalation scripts

---
+++ scripts/os_install.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install build-essential libssl-dev
+
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get install -y nodejs git
+
+sudo npm install -g grunt-cli foreman
